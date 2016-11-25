cbd01129b54d8d7223974e9461d90797827a3348
Andrea SonnY <andreasonny83@gmail.com>
2016-11-25 17:11:14 +0000

    login

--- app/routes.js
+++ app/routes.js
@@ -36,12 +36,17 @@ router
   .post('/add', add);
 
 function _init() {
-  const projectId = process.env.PROJECT_ID || serviceAccount.PROJECT_ID || 'PROJECT_ID';
   const databaseURL = process.env.DATABASE_URL || serviceAccount.DATABASE_URL || 'DATABASE_URL';
-  const clientEmail = process.env.CLIENT_EMAIL || serviceAccount.CLIENT_EMAIL || 'CLIENT_EMAIL';
-  const privateKey = process.env.PRIVATE_KEY || serviceAccount.PRIVATE_KEY || 'PRIVATE_KEY';
   const apiKey = process.env.API_KEY || serviceAccount.API_KEY || 'API_KEY';
   const authDomain = process.env.AUTH_DOMAIN || serviceAccount.AUTH_DOMAIN || 'AUTH_DOMAIN';
+  const email = process.env.EMAIL || serviceAccount.EMAIL || 'EMAIL';
+  const password = process.env.PASSWORD || serviceAccount.PASSWORD || 'PASSWORD';
+  // const projectId = process.env.PROJECT_ID || serviceAccount.PROJECT_ID || 'PROJECT_ID';
+  // const clientEmail = process.env.CLIENT_EMAIL || serviceAccount.CLIENT_EMAIL || 'CLIENT_EMAIL';
+  // const privateKey = process.env.PRIVATE_KEY || serviceAccount.PRIVATE_KEY || 'PRIVATE_KEY';
+
+  let errorCode;
+  let errorMessage;
 
   // firebase.initializeApp({
   //   credential: firebase.credential.cert({
@@ -64,25 +69,35 @@ function _init() {
   db = firebase.database();
   ref = db.ref('/quotes');
 
+  firebase.auth()
+          .signInWithEmailAndPassword(email, password)
+          .catch(function(error) {
+            errorCode = error.code;
+            errorMessage = error.message;
+          });
+
   if (debug) {
     console.log('start debug');
     console.log('databaseURL', databaseURL);
-    console.log('PROJECT_ID', projectId);
-    console.log('CLIENT_EMAIL', clientEmail);
-    console.log('PRIVATE_KEY', privateKey);
+    if (errorCode) {
+      console.log('Error during Firebase authentication:', errorCode, errorMessage);
+      console.log('Current user', firebase.auth().currentUser);
+    }
   }
 }
 
 // Private functions
 function status(req, res) {
-  var user = firebase.auth().currentUser;
-  console.log(user);
   const data = {
     app: appName,
     version: appVersion,
     status: 200,
     message: 'OK - ' + Math.random().toString(36).substr(3, 8)
   };
+
+  if (debug) {
+    console.log('Current user', firebase.auth().currentUser.uid);
+  }
 
   res.status(200).send(data);
 }
@@ -99,7 +114,6 @@ function add(req, res) {
     console.log('captcha:', captcha);
     console.log('model:');
     console.log(model);
-    console.log(ref);
   }
 
   if (!model.quote || !model.author || !model.creator || !captcha) {
