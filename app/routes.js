e601a094952780b7486a23f4c8b235c32948fdfb
andreasonny83 <andreasonny83@gmail.com>
2017-03-13 00:05:45 +0000

    Changing timestamp logic

--- app/routes.js
+++ app/routes.js
@@ -107,7 +107,7 @@ function _add(req, res) {
     quote: req.body.quote || null,
     author: req.body.author || null,
     creator: req.body.creator || null,
-    time: 0 - firebase.database.ServerValue.TIMESTAMP
+    time: - (firebase.database.ServerValue.TIMESTAMP)
   };
 
   if (!model.quote ||
